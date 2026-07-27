df8ed04f45b813ab9f433acbce3b6be5db5b7be9
umair <umair@gmail.com>
2026-07-27 17:44:56 +0500

    Remove sharp dependency, configure passthroughImageService

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://luminexstar.ae',
+  image: {
+    service: passthroughImageService(),
+  },
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
